c039630aba4b589649dd9ae19fc9a2a5044a12d8
Aman Negi <negi.aman500@gmail.com>
2025-11-13 17:15:54 +0530

    Readme

--- src/migrations/20251113080426-create-airplane.js
+++ src/migrations/20251113080426-create-airplane.js
@@ -15,7 +15,8 @@ module.exports = {
       },
       capacity: {
         type: Sequelize.INTEGER,
-        defaultValue: 0
+        defaultValue: 0,
+        validate:{max: 1000},
 
       },
       createdAt: {
